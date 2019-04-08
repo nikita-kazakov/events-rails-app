519420ad5d6f26bcba6ce0aa5faebf8721597c27
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-04-08 14:55:57 -0400

    Working with Rails Helpers

--- events/app/helpers/events_helper.rb
+++ events/app/helpers/events_helper.rb
@@ -2,4 +2,12 @@
 #Insert tutorial section #5 - Nikita.
 module EventsHelper
 
+  def format_price(event)
+    if event.price == 0
+      "<strong>Free!</strong>".html_safe
+    else
+      number_to_currency(event.price)
+    end
+  end
+
 end
