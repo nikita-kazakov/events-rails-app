a40947fe701aad345def0f3c53328fb419709fc5
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-06 14:52:05 -0400

    Forms: Ability to create events form.

--- events/config/routes.rb
+++ events/config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "events#index"
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as: "event"
-  get "events/:id/edit" => "events#edit", as: "edit_event"
-  patch "events/:id" => "events#update"
+  #get "events" => "events#index"
+  #get "events/:id" => "events#show", as: "event"
+  #get "events/:id/edit" => "events#edit", as: "edit_event"
+  #patch "events/:id" => "events#update"
+  #get "events/new" => "events#new"
 
-
+  resources :events
 end
