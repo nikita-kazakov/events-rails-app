e796f12e7b0170ee23ad4000a8aecef17d3faf67
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-09 23:26:24 -0400

    Events Validations.

--- events/app/models/event.rb
+++ events/app/models/event.rb
@@ -1,5 +1,18 @@
 class Event < ApplicationRecord
 
+  validates :name, :description, :location, presence: true
+
+  validates :description, length: {minimum: 25}
+
+  validates :price, numericality: {greater_than_or_equal_to: 0}
+
+  validates :capacity, numericality: {only_integer: true, greater_than: 0}
+
+  validates :image_file_name, allow_blank: true, format: {
+      with:    /\w+\.(gif|jpg|png)\z/i,
+      message: "must reference a GIF, JPG, or PNG image"
+  }
+
   def free?
     #price == 0
     #price.zero?
@@ -9,5 +22,6 @@ class Event < ApplicationRecord
   def self.upcoming
     where("starts_at >= ?", Time.now).order("starts_at")
   end
+
 
 end
