8a08dd05afeb3aa43a0338cc8513170bfdc9b9a5
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-23 16:45:01 -0400

    Added Registration Resource (One to Many Model)

---
+++ events/db/migrate/20190523202053_create_registrations.rb
@@ -0,0 +1,13 @@
+class CreateRegistrations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :registrations do |t|
+      t.string :name
+      t.string :email
+      t.string :string
+      t.string :how_heard
+      t.references :event, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
