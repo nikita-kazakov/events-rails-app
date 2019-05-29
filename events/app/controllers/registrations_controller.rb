dcabca830c1cbb2ff6bf128daac7508fd59b259b
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-05-29 14:25:10 -0400

    Adding Forms to Registrations

--- events/app/controllers/registrations_controller.rb
+++ events/app/controllers/registrations_controller.rb
@@ -5,4 +5,23 @@ class RegistrationsController < ApplicationController
     @registrations = @event.registrations
   end
 
+  def new
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new(params.
+        require(:registration).permit(:name, :email, :how_heard))
+
+    if @registration.save
+      redirect_to event_registrations_path(@event),
+                  notice: "Thanks, you're registered."
+    else
+      render :new
+    end
+
+  end
+
 end
